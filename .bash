3f40c6a1aafec660bbf2a1df31213200dd754800
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-06-26 13:57:46 +0300

    Create .bash

---
+++ .bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Infinite loop
+while true; do
+    # Restart server command goes here
+    echo "Restarting server..."
+    # Replace the command below with the appropriate command to restart your server
+    systemctl restart <your_server_service_name>
+
+    # Wait for 24 hours before restarting again
+    sleep 24h
+done
