3fa7f3d1f10e24fe8f40fd19472b3444ee72832c
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-08-01 17:23:18 +0300

    Update .bash

--- .bash
+++ .bash
@@ -13,6 +13,10 @@ only one W2
     sleep 24h
     # Wait for 22244 hours before restarting again
     # Wait for 23333
+The website which showcases [Pure CSS][Pure].
+//travis-ci.org/pure-css/pure-site.svg?branch=master)](https://travis-ci.org/pure-css/pure-site)
+[![Dependency Status](https://david-dm.org/pure-css/pure-sitjhghde.svg)](https://david-dm.org/pure-csscss/pure-site)
+[![Dependency Status](https://david-dm.org/pure-css/pure-sitjhghde.svg)](https://david-dm.org/pure-csscss/pure-site)
 only one W2
     sleep 24h
         # Restart server command goes here
