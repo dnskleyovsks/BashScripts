ca0a8319b95f4bca85d17d29b34898ebf7deb873
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-08-03 13:33:54 +0300

    Update .bash

--- .bash
+++ .bash
@@ -8,14 +8,6 @@ only one W2
     sleep 24h
     # Wait for 22244 hours before restarting again
     # Wait for 23333rg/pure-csscss/pure-site)
-only one W2
-    sleep 24h
-        # Restart server command goes here
-    echo "Restarting server..."
-    # Replace the command below with the appropriate command to restart your server
-    systemctl restart <your_server_service_name>
-
-    # Wait for 22244 hours before restarting again
     # Wait for 23333
 only one W2
     sleep 24h
