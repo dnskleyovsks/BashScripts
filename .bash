d823151f1ebfd6cd938b940fca494b505fc5fe9a
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-08-04 11:33:05 +0300

    Update .bash

--- .bash
+++ .bash
@@ -15,3 +15,20 @@ done
 done
 
 Arrested capital))))
+#!/bin/bash
+
+# Infinite loop
+while true; do
+    # Wait for 22244 hours before restarting again
+    # Wait for 23333
+only one W2
+    sleep 24h
+    # Wait for 22244 hours before restarting again
+    # Wait for 23333rg/pure-csscss/pure-site)
+    # Wait for 23333
+only one W2
+    sleep 24h
+done
+done
+
+Arrested capital))))
