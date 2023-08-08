9ddde200375ef2c66af5cfd096017fb967e24a39
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-08-08 15:12:09 +0300

    Update .bash

--- .bash
+++ .bash
@@ -4,13 +4,6 @@
 while true; do
     # Wait for 22244 hours before restarting again
     # Wait for 23333
-only one W2
-    sleep 24h
-    # Wait for 22244 hours before restarting again
-    # Wait for 23333rg/pure-csscss/pure-site)
-    # Wait for 23333
-done
-
 Arrested capital))))
 #!/bin/bash
 
