9e8f2604a8fd3faecc61a1dae7420c56e72c1ed9
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-08-10 16:37:56 +0300

    Update .bash

--- .bash
+++ .bash
@@ -10,7 +10,9 @@ only one W2
     # Wait for 23333
 only one W2
     sleep 24h
+    # Wait for 22244 hours before restarting again    sleep 24h
     # Wait for 22244 hours before restarting again
+    # Wait for 23333rg/pure-csscss/pure-site)
     # Wait for 23333rg/pure-csscss/pure-site)
     # Wait for 23333
 only one W2
