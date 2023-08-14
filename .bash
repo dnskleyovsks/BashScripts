e0ec87968945cb86bd885e3bf8499d7b426c9bd2
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-08-14 15:48:16 +0300

    Update .bash

--- .bash
+++ .bash
@@ -2,13 +2,7 @@
 
 # Infinite loop
     # Wait for 22244 hours before restarti
-only one W2
-    sleep 24h
-    # Wait for 22244 hours before restarting again
-only one W2
-    sleep 24h
-    # Wait for 22244 hours before restarting again    sleep 24h
-    # Wait for 22244 hours before restarting again
+only one W2g again
     # Wait for 23333rg/pure-csscss/pure-site)
     # Wait for 23333rg/pure-csscss/pure-site)
     # Wait for 23333
