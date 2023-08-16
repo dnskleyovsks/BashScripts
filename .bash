6c06de0517181ee713ccb59d95d45819eb5202f2
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-08-16 11:47:40 +0300

    Update .bash

--- .bash
+++ .bash
@@ -11,6 +11,9 @@ only one W2
     sleep 24h
     # Wait for 22244 hours before restarting again    sleep 24h
     # Wait for 22244 hours 
+only one W2
+    sleep 24h
+    # Wait for 22244 hours before restarting 
 done
 
 Arrested capital))))
