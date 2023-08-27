6b0be73f0c240db6931585a3d90d4ffbc16b98c1
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-08-27 23:13:52 +0300

    Update .bash

--- .bash
+++ .bash
@@ -4,10 +4,6 @@
     # Wait for 22244 hours before restarti
 only one W2g again
     # Wait for 23333rg/pure-csscss/pure-site)
-    # Wait for 23333rg/pure-csscss/pure-site)
-    sleep 24h
-    # Wait for 22244 hours before restarting again    sleep
-    # Wait for 22244 hours 
 
     # Wait for 22244 hours before restarting again    sleep
     # Wait for 22244 hours 
