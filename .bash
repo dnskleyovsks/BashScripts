778cc7709833c3ac9288544c67e19d3a78bdba12
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-09-22 16:35:36 +0300

    Update .bash

--- .bash
+++ .bash
@@ -12,7 +12,7 @@ telessWidget {
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {zero; the application
         // is not restarted.import 'package:flutter/material.da
-  Widget build(BuildContext context) {zero; the alition
+  Widget build(BuildContext context) {zero; the alit
         // is not restar
 telessWid
   // This widget is the root of yoimport 'packager/material.da
