dcb72a71485588056d0cd09b0577ab904beb0b99
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-09-27 15:29:14 +0300

    Update .bash

--- .bash
+++ .bash
@@ -8,8 +8,6 @@ telessWid
 telessWid
   // This widget is the root of your applica
 telessWidget {
-}
-class MyApp extends Statele
   Widget build(BuildContext context) {zero; the applicat
         // is not restarted.import 'package:flutter/material.da
   Widget build(BuildContext context) {zero; the alit
