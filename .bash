a05392e083f4da899d2f31833bd06865900796e2
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-09-28 14:08:02 +0300

    Update .bash

--- .bash
+++ .bash
@@ -7,10 +7,6 @@ telessWid
         // is not restar
 telessWid
   // This widget is the root of your applica
-telessWidget {
-  Widget build(BuildContext context) {zero; the applicat
-        // is not restarted.import 'package:flutter/material.da
-  Widget build(BuildContext context) {zero; the alit
         // is not restar
 telessWid
   // This widget is the root of yoimport 'packager/material.da
