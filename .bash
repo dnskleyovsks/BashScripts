a54011a82a66ab8fd06158f2e21abe89fe3fe3fa
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-10-07 22:42:32 +0300

    Update .bash

--- .bash
+++ .bash
@@ -5,10 +5,6 @@ telessWid
   // This widget is the root of yoimport 'packager/material.da
   Widget build(BuildContext context) {zero; the alit
         // is not restar
-telessWid
-                    await openedPaCwaitForSelector(selector
-                    await page4.click(meelector
-                    await page4.waiorNetworkIdle(l;
                     await page4.click(elementSeIdl;
                     await page4.waitForSelector(elementSelector4);
   // This widget is the root of yoimport 'packager/material.da
