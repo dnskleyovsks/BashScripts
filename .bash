a0f73bc284b6bf9026365cfe8eb7052b012cca5c
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-10-19 16:12:24 +0300

    Update .bash

--- .bash
+++ .bash
@@ -11,9 +11,6 @@ count_number;= data.profiles[i].id;
   @overr
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
-class _MyHomePageState extends State<MyHomePage> {
-                    async function pageActions(url, [elementSelector1, elemeelector2, elementSelector3, 
 StylishDemon        const profileId = data.iles[i        const profileId = data.ofiles[i].id;
         const accountNumber = daa.profiles[i].account_numb
         const profilePassword = data.profiles[i].password;
