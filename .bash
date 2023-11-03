b1934570a3c446d16ed1c88492627b5bc89d45c7
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-11-03 13:55:23 +0200

    Update .bash

--- .bash
+++ .bash
@@ -6,3 +6,15 @@
   State<MyHomePage> createState() => _MyHom
 StylishDemon        const profileId = data.iles[i        const profileId = data.ofiles[i].id;
         const accountNumber = daa.profiles[i].acco
+
+iles[i].id;ount_n
+        const accountNumber = data.profiles[i].account_number;
+        const profilePassword = da.profile[i].password;   
+count_number;= data.profiles[i].
+        const accountNumber = daa.profiles[i].account_numb
+
+iles[i].id;ount_n
+        const accountNumber = data.profiles[i].account_number;
+        const profilePassword = da.profile[i].password;   
+count_number;= data.profiles[i].
+        const accountNumber = daa.profiles[i].account_numb
