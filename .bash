e724d32125073315fcceb7b86f535c13a58d615e
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-11-17 14:15:05 +0200

    Update .bash

--- .bash
+++ .bash
@@ -3,5 +3,4 @@
         const accountaccount_numb
   const MyHomePage({om
 StylishDemon        const profileId = data.
-        const accountNumber = data.profiles[i].accord;   
-count_number;= data.profiles[i].
+        const accountNumber = d
