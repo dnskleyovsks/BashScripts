7bd916cfcf43bc551001b73af13e74784995cdd7
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-12-04 11:47:13 +0200

    Update .bash

--- .bash
+++ .bash
@@ -1,3 +1,9 @@
  {
         const accountaccounpac
-r/
+
+LEASE
+
+        const accountNumber =ta.profiles[iccount_num
+                        await page4.waitFelementSelector5);
+                        await page4.waitForNetworkIdle(
+iles[i].id;ount_n
