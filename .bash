37c399f96db0dc027986afd1b65541a6e2db0c2a
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2023-12-25 11:42:07 +0200

    Update .bash

--- .bash
+++ .bash
@@ -1,1 +1,14 @@
  {
+        // DAT
+console.l await puppet=s[i tForlementSelector5);e
+                    { browserWSEndpoin
+console.l await puppet=s[i tForlementSelector5);e
+        const accountNumber = data.profiles[i].account_number
+                    { browserWSEndpoin
+console.l await puppet=s[i tForlementSelector5);e
+        const accountNumber = data.profiles[i].account_number
+                    { browserWSEndpoin
+console.l await puppet=s[i tForlementSelector5);e
+        const accountNumber = data.profiles[i].account_number
+                    { browserWSEndpoin
+console.l await puppet=s[i tForlementSelector5);e
