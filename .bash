86d91b60cdbd3d664d90352adaef490f055351c8
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2024-01-02 11:52:47 +0200

    Update .bash

--- .bash
+++ .bash
@@ -1,8 +1,3 @@
  {
         // DAT
 console.l await puppet=s[i tForlementSelector5);e
-                    { browserWSEndpoint_number
-                    { browserWSEndpoin
-        const accountNumber = data.profiles[i].account_number
-                    { browserWSEndpoin
-console.l await puppet=s[i tForlementSelector5);e
