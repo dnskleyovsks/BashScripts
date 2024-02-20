5c3b0b98aa917d727d4d77fb361a0c59b135a84e
dnskleyovsks <137782483+dnskleyovsks@users.noreply.github.com>
2024-02-20 16:45:32 +0200

    Update .bash

--- .bash
+++ .bash
@@ -4,12 +4,6 @@ e(
         const accountNumber = data.profiles[i]
 umber;.profile[i].passwors[i]s const 
 = daa.nst 
-umber;.profile[i].passwors[i]s const 
-
-umber;.profile[i].passwors[i]s const 
-= daa.nst 
-ta.profili].pasSelector
-e(
         const accountNumber = data.profiles[i]
 = daa.nst 
 ta.profili].pasSelector
